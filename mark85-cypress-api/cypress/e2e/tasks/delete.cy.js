d6d17cbb6478b73f8bdbd020bcf7580df6b5db9c
leonardopadilha <leonardopadilha28@gmail.com>
2024-07-15 12:44:10 -0300

    Adicionando cenário de delete

---
+++ mark85-cypress-api/cypress/e2e/tasks/delete.cy.js
@@ -0,0 +1,59 @@
+/// <reference types="cypress" />
+
+describe('DELETE /tasks/:id', () => {
+    beforeEach(function () {
+        cy.fixture('tasks/delete').then(function(tasks) {
+            this.tasks = tasks
+        })
+    });
+
+    it('remove a task', function () {
+        const { user, task } = this.tasks.remove
+
+        cy.task('removeTask', task.name, user.email)
+        cy.task('removeUser', user.email)
+        cy.postUser(user)
+
+        cy.postSession(user)
+            .then(userResp => {
+                
+                cy.postTask(task, userResp.body.token)
+                    .then(respTask => {
+
+                        cy.deleteTask(respTask.body._id, userResp.body.token)
+                            .then(response => {
+                                expect(response.status).to.eq(204)
+                        })
+                    })
+
+            })
+    })
+
+    it('task not found after deletion', function () {
+        const { user, task } = this.tasks.not_found
+
+        cy.task('removeTask', task.name, user.email)
+        cy.task('removeUser', user.email)
+        cy.postUser(user)
+
+        cy.postSession(user)
+            .then(userResp => {
+                
+                cy.postTask(task, userResp.body.token)
+                    .then(respTask => {
+
+                        cy.deleteTask(respTask.body._id, userResp.body.token)
+                            .then(response => {
+                                expect(response.status).to.eq(204)
+                        })
+
+                        cy.deleteTask(respTask.body._id, userResp.body.token)
+                            .then(response => {
+                                expect(response.status).to.eq(404)
+                        })
+                    })
+
+            })
+    })
+        
+})
