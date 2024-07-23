4e6bebfb568e1958d812bc594b40952da5cd2d1e
leonardopadilha <leonardopadilha28@gmail.com>
2024-07-23 15:49:27 -0300

    Arquivos put

---
+++ mark85-cypress-api/cypress/e2e/tasks/put.cy.js
@@ -0,0 +1,64 @@
+/// <reference types="cypress" />
+
+describe('PUT /tasks/:id/done', () => {
+    beforeEach(function () {
+        cy.fixture('tasks/put').then(function(tasks) {
+            this.tasks = tasks
+        })
+    });
+
+    it('update task to done', function () {
+        const { user, task } = this.tasks.update
+
+        cy.task('removeTask', task.name, user.email)
+        cy.task('removeUser', user.email)
+        cy.postUser(user)
+
+        cy.postSession(user)
+            .then(userResp => {
+                
+                cy.postTask(task, userResp.body.token)
+                    .then(respTask => {
+
+                        cy.putTaskDone(respTask.body._id, userResp.body.token)
+                            .then(response => {
+                                expect(response.status).to.eq(204)
+                            })
+
+                        cy.getUniqueTask(respTask.body._id, userResp.body.token)
+                            .then(response => {
+                                expect(response.status).to.eq(200)
+                                expect(response.body.is_done).to.true
+                        })
+                    })
+
+            })
+    })
+
+    it('task updated not found', function () {
+        const { user, task } = this.tasks.not_found
+
+        cy.task('removeTask', task.name, user.email)
+        cy.task('removeUser', user.email)
+        cy.postUser(user)
+
+        cy.postSession(user)
+            .then(userResp => {
+                
+                cy.postTask(task, userResp.body.token)
+                    .then(respTask => {
+
+                        cy.deleteTask(respTask.body._id, userResp.body.token)
+                            .then(response => {
+                                expect(response.status).to.eq(204)
+                        })
+
+                        cy.putTaskDone(respTask.body._id, userResp.body.token)
+                            .then(response => {
+                                expect(response.status).to.eq(404)
+                            })
+                    })
+            })
+
+    })
+})
